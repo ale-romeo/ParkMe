bfca2fbd64e0a62af6c5c0e3dc08bb8a51cf329a
aleromeo <alexromeo160@gmail.com>
2022-12-28 02:00:55 +0100

    resp

--- db_proj/php/src/index.php
+++ db_proj/php/src/index.php
@@ -8,27 +8,27 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <div class="header">
-        <img src="img/logo.png" alt="logo" />
-    </div>
     <div class="container">
-        <?php
-        if (isset($_SESSION["error"])) {
-            echo "<h2>Welcome</h2>
-            <p>" . $_SESSION["error"] . " Try again.</p>";
-            unset($_SESSION["error"]);
-        }else{
-            echo "<h2>Welcome</h2>
-            <p>Please log in to access the system:</p>";
-        }
-        ?>
-        <form action="logged.php" method="post">
-            <label>Username:</label><br>
-            <input type="text" name="username" required><br>
-            <label>Password:</label><br>
-            <input type="password" name="password" required><br>
-            <input type="submit" name="login" value="Log in">
-        </form>
+        <img src="img/logo.png" alt="logo" />
+        <div class="loginbox">
+            <?php
+            if (isset($_SESSION["error"])) {
+                echo "<h2>Welcome</h2>
+                <p>" . $_SESSION["error"] . " Try again.</p>";
+                unset($_SESSION["error"]);
+            }else{
+                echo "<h2>Welcome</h2>
+                <p>Please log in to access the system:</p>";
+            }
+            ?>
+            <form action="logged.php" method="post">
+                <label>Username:</label><br>
+                <input type="text" name="username" required><br>
+                <label>Password:</label><br>
+                <input type="password" name="password" required><br>
+                <input type="submit" name="login" value="Log in">
+            </form>
+        </div>
     </div>
 </body>
 </html>

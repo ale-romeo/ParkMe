b1c23ace86ea5774714410259c4979a647be57b4
Peppe <djpinos4@gmail.com>
2023-01-11 10:33:09 +0100

    pino

--- db_proj/php/src/upload.php
+++ db_proj/php/src/upload.php
@@ -23,7 +23,7 @@ if($_FILES["fileToUpload"]["size"] > 500000){
 
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  echo "Sorriy, only JPG, JPEG, PNG & GIF files are allowed.";
   $uploadOk = 0;
 }
 

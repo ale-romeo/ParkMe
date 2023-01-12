0c3408f3d55ad3ae3eb714758ba5e996b5dbd452
aleromeo <alexromeo160@gmail.com>
2023-01-12 01:24:39 +0100

    qrcode2try

--- db_proj/php/src/js/land.js
+++ db_proj/php/src/js/land.js
@@ -18,3 +18,33 @@ function getTicket() {
       }
     });
 }
+
+let qrContentInput = document.getElementById("qr-content");
+let qrGenerationForm = document.getElementById("qr-generation-form");
+let qrCode;
+
+function generateQrCode(qrContent) {
+  return new qrCode("qr-code", {
+    text: qrContent,
+    width: 256,
+    height: 256,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: qrCode.CorrectLevel.H,
+  });
+}
+
+// Event listener for form submit event
+qrGenerationForm.addEventListener("submit", function (event) {
+  // Prevent form submission
+  event.preventDefault();
+  let qrContent = qrContentInput.value;
+  if (qrCode == null) {
+    // Generate code initially
+    qrCode = generateQrCode(qrContent);
+  } else {
+    // If code already generated then make
+    // again using same object
+    qrCode.makeCode(qrContent);
+  }
+});
--- db_proj/php/src/upload.php
+++ db_proj/php/src/upload.php
@@ -1,41 +1,58 @@
-<?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  }else{
-    echo "File is not an image.";
-    $uploadOk = 0;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="js/land.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
+  <title>QR Code Generator</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="loginbox">
+    <img src='img/logo.png' alt='logo'/>
+    <header>
+        <h1>Inserire codice univoco:</h1>
+    </header>
+    <main>
+      <form id="qr-generation-form">
+        <input type="text" id="user_id" name="user_id" placeholder="Enter user ID" autocomplete="off"/>
+        <input type="submit" value="Generate QR Code"/>
+      </form><br/>
+      <div id="qr-code"></div>
+    </main>
+  </div>
+  <?php
+  require_once('vendor/autoload.php');
+  // check if the form has been submitted
+  if (isset($_GET['user_id'])) {
+    // get the user's unique identifier from the query string
+    $user_id = $_GET['user_id'];
+    // create a QR code with the user's unique identifier
+    $qrCode = new \Endroid\QrCode\QrCode($user_id);
+    // change the size and color of the QR code
+    $qrCode->setSize(300);
+    $qrCode->setForegroundColor(QrCode::COLOR_BLACK);
+
+    // display the QR code
+    echo '<img src="data:'.$qrCode->getContentType().';base64,'.$qrCode->writeString().'" />';
   }
-}
-
-// Check file size
-if($_FILES["fileToUpload"]["size"] > 500000){
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-}else{
-  if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)){
-    echo "The file ".htmlspecialchars(basename($_FILES["fileToUpload"]["name"]))." has been uploaded.";
-  }else{
-    echo "Sorry, there was an error uploading your file.";
-  }
-}
-?>
+  ?>
+</body>
+<footer>
+  <div class="footer">
+    <div class="left">
+      <p class="copyright">Copyright 2022</p>
+    </div>
+    <div class="center">
+      <ul>
+        <li><a href="mailto:romeoalessandroing@gmail.com" class="contact">Contact</a></li>
+      </ul>
+    </div>
+    <div class="right">
+      <p class="author">Alessandro Romeo</p>
+    </div>
+  </div>
+</footer>
+</html>

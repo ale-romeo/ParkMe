048018aabe32a05926e35d64fa9de247883607ef
aleromeo <alexromeo160@gmail.com>
2023-01-14 04:50:59 +0100

    new

--- src/js/land.js
+++ src/js/land.js
@@ -2,49 +2,23 @@ const togglePassword = document.querySelector('#togglePassword');
 const password = document.querySelector('#password');
 
 togglePassword.addEventListener("click", function () {
-    this.classList.toggle("fa-eye-slash");
-    const type = password.getAttribute("type") === "password" ? "text" : "password";
-    password.setAttribute("type", type);
+  this.classList.toggle("fa-eye-slash");
+  const type = password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
 })
 
 function getTicket() {
-    $.ajax({
-      url: 'guest.php',
-      type: 'POST',
-      data: {Park: true},
-      success: function(response) {
-        // Update the page with the response from the PHP script
-        alert(response);
-      }
-    });
-}
-
-let qrContentInput = document.getElementById("qr-content");
-let qrGenerationForm = document.getElementById("qr-generation-form");
-let qrCode;
-
-function generateQrCode(qrContent) {
-  return new qrCode("qr-code", {
-    text: qrContent,
-    width: 256,
-    height: 256,
-    colorDark: "#000000",
-    colorLight: "#ffffff",
-    correctLevel: qrCode.CorrectLevel.H,
+  $.ajax({
+    url: 'guest.php',
+    type: 'POST',
+    data: { Park: true },
+    success: function (response) {
+      // Update the page with the response from the PHP script
+      alert(response);
+    }
   });
 }
 
-// Event listener for form submit event
-qrGenerationForm.addEventListener("submit", function (event) {
-  // Prevent form submission
-  event.preventDefault();
-  let qrContent = qrContentInput.value;
-  if (qrCode == null) {
-    // Generate code initially
-    qrCode = generateQrCode(qrContent);
-  } else {
-    // If code already generated then make
-    // again using same object
-    qrCode.makeCode(qrContent);
-  }
+$('.navbar-toggler').on('click', function () {
+  $('#dropdown-menu').toggleClass('dropdown-menu-right');
 });
--- src/testing.php
+++ src/testing.php
@@ -1,37 +1,60 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <title>Smartphone Parking App</title>
+  <title>ParkME</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="styletest.css">
   <link rel="shortcut icon" href="img/favicon.ico">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="js/land.js"></script>
 </head>
+
 <body>
 
-<div class="jumbotron text-center">
-  <h1>Smartphone Parking App</h1>
-  <p>Find and reserve parking spots with ease</p> 
-</div>
+  <nav class="navbar navbar-expand-sm fixed-top navbar-light">
+    <a class="navbar-brand" href="#"><img id="logo" src="img/ParkME-logos_white.png" alt="Logo"></a>
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto" id="dropdown-menu">
+          <li class="nav-item">
+            <a class="nav-link" href="#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#services">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
-<div class="container">
-  <div class="row">
-    <div class="col-sm-4">
-      <h3>Search for Parking</h3>
-      <p>Easily search for available parking spots by location, time, and price.</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Reserve a Spot</h3>
-      <p>Reserve a parking spot in advance to guarantee your spot and avoid the hassle of searching for parking.</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Navigate to Your Spot</h3>
-      <p>Use our app's navigation feature to easily find your reserved spot and park with confidence.</p>
+  <div class="container-fluid my-5">
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <h1 class="text-white pt-5">Welcome to our parking app</h1>
+        <p class="text-white pt-3">We offer an easy way to find, reserve and navigate to parking spots</p>
+        <div class="text-center pt-3">
+          <a href="login.php" class="btn btn-primary mr-2">Login</a>
+          <a href="signup.php" class="btn btn-secondary">Signup</a>
+        </div>
+      </div>
     </div>
   </div>
-</div>
+
 
 </body>
+
 </html>
